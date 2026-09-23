0757359b951415bb9407dedac265a3a3bdc00d67
Rumeysa Aydın <aydinrumeysa478@gmail.com>
2026-09-23 18:03:20 +0300

    feat: add GET /hello/:name route returning dynamic greeting

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get('/hello', (req, res) => {
   res.send('hello, world');
 });
 
+// GET /hello/{name} "hello, name"
+app.get('/hello/:name', (req, res) => {
+  res.send(`hello, ${req.params.name}`);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
